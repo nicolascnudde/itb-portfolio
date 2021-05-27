05c3e8bb742007a93c3194335eebfe304e9f7f40
pgm-nicolascnude <nicocnud@student.arteveldehs.be>
2021-05-27 09:03:27 +0200

    aInstalled NodeJS and 11ty

---
+++ .eleventy.js
@@ -0,0 +1,28 @@
+const pluginNavigation = require('@11ty/eleventy-navigation');
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.setUseGitIgnore(false);
+  eleventyConfig.addPlugin(pluginNavigation);
+
+  eleventyConfig.setDataDeepMerge(true);
+
+  eleventyConfig.addPassthroughCopy({ 'site/_static/css': 'static/css' });
+  eleventyConfig.addPassthroughCopy({ 'site/_static/images': 'static/images' });
+  eleventyConfig.addPassthroughCopy({ 'site/_static/js': 'static/js' });
+  eleventyConfig.addPassthroughCopy({ 'site/humans.txt': 'humans.txt' });
+
+  return {
+    dir: {
+      input: "site",
+      output: "docs",
+      includes: "_includes",
+      data: "_data",
+    },
+    pathPrefix: "/itb-portfolio",
+    templateFormats: ["html", "liquid", "md", "njk"],
+    markdownTemplateEngine: "liquid",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    passthroughFileCopy: true,
+  };
+}
